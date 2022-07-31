f5f2d2425ab848a5b87ffd05c03afe539e04a72d
Sean Finan <sean.finan@tch.harvard.edu>
2022-07-31 12:19:57 -0400

    Update start-viz.bat
    Silence Warning messages,
    Information for User and Pause

--- DeepPhe-win-scripts/start-viz.bat
+++ DeepPhe-win-scripts/start-viz.bat
@@ -1,15 +1,66 @@
-TITLE DeepPhe-Viz Client/Server, Ctrl-C to Close
+@ECHO OFF
+TITLE DeepPhe-Viz Update and Run, Ctrl-C to Close
 
+echo ===========
+echo DeepPhe-Viz
+echo ===========
+echo .
+echo This script updates required external applications and starts the DeepPhe Visualizer.
+echo The DeepPhe Visualizer interacts with the Neo4j Server installed with DeepPhe.
+echo If you have not started the Neo4j Server please do so now.
+echo .
+echo After the external applications have been updated DeepPhe-Viz will start.
+echo A second process will be launched, labeled "npm start".
+echo When you are done with DeepPhe-Viz, open the "npm start" window and type Ctrl-C to close it.
+echo .
+echo You must also type Ctrl-C to close this window.
+
+ECHO Stopping any currently running DeepPhe-Viz processes ...
 FOR /F "tokens=5 delims= " %%P IN ('netstat -a -n -o ^| findstr ":3000.*LISTENING 3001.*LISTENING"') DO taskkill /F /PID %%P
 
-set npm_config_loglevel=silent npm version
+:: Turn off logging except for errors. This may be a bad idea.  The default value is "notice".  The _progress option controls progress bars.
+set npm_config_loglevel=error
+
+:: Set nodejs to run in production mode only.  That should decrease installed dependencies.
+SET NODE_ENV=production
+
+::npm config
+
 set PORT=3001
+
+::Consider using a check on installation having already been performed and if not use PAUSE command.
+
+ECHO .
+ECHO Updating the DeepPhe-Viz to Neo4j Connection External Application Components.
+ECHO Some components may take a long time to update.  Please be patient.
+ECHO.
+PAUSE
 cd api
 call npm install --force
+::call npm install
+echo %ERRORLEVEL%
+
+ECHO .
+ECHO Updating the DeepPhe-Viz Client External Application Components.
+ECHO Some components may take a long time to update.  Please be patient.
+ECHO.
 cd ../client
 call npm install --force
+
+:: Consider using dos start /b (no new window) with npm log_level silent or logging redirected to a file.
+ECHO .
+ECHO Starting the DeepPhe-Viz to Neo4j Connection ...
+ECHO.
 cd ../api
-start npm start
-set PORT=3000
+start "DeepPhe-Viz to Neo4j Connection, Ctrl-C to Close" /min cmd /c npm start
+
+ECHO .
+ECHO Starting the DeepPhe-Viz Client ...
+ECHO.
+
 cd ../client
-start npm start
+set PORT=3000
+TITLE "DeepPhe-Viz Client"
+npm start
+
+EXIT
