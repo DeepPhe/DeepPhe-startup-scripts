bc56c7751cd03197b3d6945f48ae41e48c24a1f7
Sean Finan <sean.finan@tch.harvard.edu>
2022-08-08 10:26:22 -0400

    clean messages

--- DeepPhe-win-scripts/start-neo4j.bat
+++ DeepPhe-win-scripts/start-neo4j.bat
@@ -6,20 +6,21 @@ SET "PATH=NEO4J_JAVA_VAR\bin;%PATH%"
 echo ========================
 echo Neo4j Server for DeepPhe
 echo ========================
-echo .
+echo.
 echo This script starts the Neo4j Server that uses a custom plugin for DeepPhe.
-echo .
+echo.
 echo Some error logs will be saved to file, but the majority of logging can be
 echo viewed in this window.
-echo .
+echo.
 echo The Neo4j server must be running during DeepPhe document processing 
 echo and while running the DeepPhe Visualizer.
-echo .
+echo.
 echo Startup is complete when you see a line indicating that a Remote interface is available.
-echo .
+echo.
 echo To stop the server, click inside this window and press Control-C.
-echo .
+echo.
 pause
+echo.
 
 cd neo4j-community-3.5.12\bin
 neo4j.bat console
