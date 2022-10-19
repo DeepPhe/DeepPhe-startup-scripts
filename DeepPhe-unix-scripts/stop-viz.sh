9f2d5fb9f063cc939589621724523511c49664ac
John <john.d.levander@gmail.com>
2022-10-19 02:32:02 -0400

    Update stop-viz.sh

--- DeepPhe-unix-scripts/stop-viz.sh
+++ DeepPhe-unix-scripts/stop-viz.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 pid=$(<viz.pid)
 pkill 15 $pid
+pkill -f DeepPheVizApi
+pkill -f DeepPheVizClient
+pkill -f dphe-viz-launcher-1.0-jar-with-dependencies
+pkill -f start-viz.sh
+
 rm viz.pid
