01931a8600ec635be53e3f15c6fd684443559b79
John <john.d.levander@gmail.com>
2022-10-24 18:52:11 -0400

    Update stop-viz.bat

--- DeepPhe-win-scripts/stop-viz.bat
+++ DeepPhe-win-scripts/stop-viz.bat
@@ -1,4 +1,3 @@
 @echo off
 title StopViz
-taskkill /im DeepPheVizClient.exe /f
-taskkill /im DeepPheVizApi.exe /f
+wmic Path win32_process Where "CommandLine Like '%dphe-viz-launcher-1.0-jar-with-dependencies.jar%'" Call Terminate
