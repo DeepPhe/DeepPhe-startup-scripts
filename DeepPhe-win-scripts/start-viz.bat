2d79ced8251b751bfbdde35af5399f4ca88aff32
John <john.d.levander@gmail.com>
2022-10-24 17:28:30 -0400

    Update start-viz.bat

--- DeepPhe-win-scripts/start-viz.bat
+++ DeepPhe-win-scripts/start-viz.bat
@@ -1,4 +1,2 @@
 @ECHO OFF
-TITLE StartViz
-"_installer_java_home_dir\bin\java" -jar "%~dp0dphe-viz-launcher-1.0-jar-with-dependencies.jar" viz.cfg
-start /max http://127.0.0.1:3000
+TITLE StartViz "_installer_java_home_dir\bin\java" -jar "%~dp0dphe-viz-launcher-1.0-jar-with-dependencies.jar" viz.cfg
