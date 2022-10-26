f9691ce21e7b1376306457f8ba3acac2f872c8a3
John <john.d.levander@gmail.com>
2022-10-26 00:58:28 -0400

    Update start-viz.sh

--- DeepPhe-unix-scripts/start-viz.sh
+++ DeepPhe-unix-scripts/start-viz.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export JAVA_HOME=installer_java_home_dir
+export JAVA_HOME=_installer_java_home_dir
 sh -c 'echo $$ > viz.pid; exec java -jar dphe-viz-launcher-1.0-jar-with-dependencies.jar viz.cfg'
