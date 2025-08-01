eaf79ecceba528591eb9dbcde3b33bcb8a5055cb
John <john.d.levander@gmail.com>
2025-08-01 13:48:44 -0400

    changing from localhost to 127.0.0.1, which seems to work better

--- DeepPhe-unix-scripts/start-viz.sh
+++ DeepPhe-unix-scripts/start-viz.sh
@@ -2,4 +2,4 @@
 export JAVA_HOME=_installer_java_home_dir
 sh -c 'echo $$ > viz.pid; exec java -jar dphe-viz-launcher-1.0-jar-with-dependencies.jar viz.cfg'
 sleep 5
-open http://localhost:3000
+open http://127.0.0.1:3000
